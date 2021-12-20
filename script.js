3b6cec5efc9dbc746060ce6b0b5c14e7d5db7579
ricardonyc <rvardo98@gmail.com>
2021-12-19 19:03:10 -0500

    selected buttons dom

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-// Search through the 311 call data provided by the NYC API 
-// and display all the complaints made to the NYPD, filtered by borough
-// they also want to see how the police responded to the complaint
-
-// Data send after making a fetch call is formatted as JSON
-
-// only some key-value pairs are of use to us
-//   - borough
-//   - descriptor (which says what kind of complaint was made)
-//   - agency: "NYPD" (because we only want complaints that were handled by the police department)
-//   - resolution_description (which says how the police handled the complaint)
-
 // Requirements
 //   - 5 buttons representing boroughs
 //   - Input box where it takes a number to see NUMBER OF COMPLAINTS
 //   - when one of the 5 buttons are clicked, the number of complaints should be displayed on page according to NUM of
-//     complaints and borough they clicked on 
+//     complaints and borough they clicked on
 //     - IF the user doesn't input any number, make the default be 10
 //     - they only want complaints that were handled by the NYPD (consider filtering for specific "agency" when making the API call)
 //     - When the list of complaints show, each complaint should also have a button next to it that reads "see police response"
 //     - When the user clicks on "see police response" button, it should toggle how the police responded to that complaint
 //     - Make sure it only toggles the response for that ONE complaint, not the entire list!
 
+const buttons = document.querySelectorAll("button");
 
-// get value of input (NUMBER of complaints)
-// when button is clicked, display the NUMBER of complaints in THAT borough
-// no input NUMBER ? set value to default 10
-// ONLY complaints that were handled by the NYPD
+buttons.forEach((borough) => {
+  borough.addEventListener("click", () => {
+    const inputNum = 10;
+    if (document.getElementById("user-input").value !== "") {
+      inputNum = document.getElementById("user-input").value;
+    }
+  });
+});
 
+// const api = (borough, inputNum) => {
+//   const url = `https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=${borough}&agency=NYPD`;
 
+//   fetch(url)
+//     .then(response => response.json())
+//     .then()
 
-const userInput = document.querySelector("input")
-const brooklyn = document.getElementById("brooklyn")
+// };
 
-brooklyn.addEventListener("click", () => {
-    let complaints = userInput.value
-    const url = `https://data.cityofnewyork.us/resource`
-})
+document.getElementById("fetch").addEventListener("click", getData());
+
+function getData() {
+  fetch(
+    `https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BRONX&agency=NYPD`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      document.getElementById("output-container").innerHTML = JSON.stringify(data);
+    });
+}
